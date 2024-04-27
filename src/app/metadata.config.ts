7b416f7671411457c7a7a5c3efc1478ee0c599d9
junayed-code <junayed.code@gmail.com>
2024-04-27 11:49:22 +0600

    Initialize project

---
+++ src/app/metadata.config.ts
@@ -0,0 +1,8 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Frontend Mentor - Interactive comments section solution",
+  description:
+    "This is a solution for the Interactive comments section on the Frontend Mentor.",
+  icons: [{ rel: "icon", url: "/favicon.png", type: "image/png" }],
+};
