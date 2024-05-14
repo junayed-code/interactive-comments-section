63ef08dd71e47dd99558fe121226c2226adc288f
junayed-code <junayed.code@gmail.com>
2024-05-14 11:33:15 +0600

    Update the metadata.config.ts file

--- src/app/metadata.config.ts
+++ src/app/metadata.config.ts
@@ -1,8 +1,22 @@
 import type { Metadata } from "next";
 
+const title =
+  "Interactive comments section solution - Frontend Mentor | Junayed Akbor";
+const description =
+  "This is a solution for the Interactive comments section by Junayed Akbor on the Frontend Mentor.";
+
 export const metadata: Metadata = {
-  title: "Frontend Mentor - Interactive comments section solution",
-  description:
-    "This is a solution for the Interactive comments section on the Frontend Mentor.",
+  title,
+  description,
+  metadataBase: new URL("https://interactive-comments-segment.netlify.app"),
   icons: [{ rel: "icon", url: "/favicon.png", type: "image/png" }],
+  openGraph: {
+    title,
+    description,
+    url: "/",
+    type: "website",
+    locale: "en_US",
+    siteName: "Interactive comments section",
+    images: { type: "image/png", url: "/og.png", width: 1515, height: 852 },
+  },
 };
